ea8785470cdf1dc7576d66eedd9b3e01c750b5cd
unknown <saidasfour20@gmail.com>
2021-03-19 03:26:20 -0700

    first commit

---
+++ test/flut/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+main() {
+  runApp(MaterialApp(
+    home:Scaffold(
+      appBar:AppBar(
+title: Text('First app'),
+      ),
+      body: Center(
+        child:Text("my first app",
+        style:TextStyle(fontSize: 40))
+      ),)
+  ));
+}
